049fd10243336ee35a11735fad902f041c821a0e
kave haj <kave.haj@snapp.cab>
2023-02-23 18:37:32 +0330

    added echo

--- api/handler/resolve.go
+++ api/handler/resolve.go
@@ -1,32 +1,29 @@
 package handler
 
 import (
+	"net/http"
 	"url-shortenr/database"
 
 	"github.com/go-redis/redis/v8"
-
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func ResolverURL(c *fiber.Ctx) error {
-	url := c.Params("url")
+func ResolverURL(c echo.Context) error {
+
+	url := c.Param("url")
 	r := database.CreateClient(0)
 	defer r.Close()
 
 	value, err := r.Get(database.Ctx, url).Result()
 	if err == redis.Nil {
-		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
-			"error": "short not found in database",
-		})
+		return c.String(http.StatusNotFound, "short not found in database")
 	} else if err != nil {
-		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": "can't connect to database",
-		})
+		return c.String(http.StatusInternalServerError, "can't connect to database")
 	}
 
 	rInr := database.CreateClient(1)
 	defer rInr.Close()
 	_ = rInr.Incr(database.Ctx, "counter")
 
-	return c.Redirect(value, 301)
+	return c.Redirect(301, value)
 }
